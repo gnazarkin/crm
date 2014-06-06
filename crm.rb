d530bc6e4cd5eed634a3b055914d8276a611d6dc
gnazarkin <gleb.nazarkin@gmail.com>
2014-06-06 12:01:20 -0400

    Finished modify and add methods in the CRM class

--- crm.rb
+++ crm.rb
@@ -16,8 +16,6 @@ class CRM
 		puts "\e[H\e[2J"
 	end
 
-
-
 	def print_main_menu
 		puts "[add] Add a new contact"
  	  puts "[modify] Modify an existing contact"
@@ -30,15 +28,81 @@ class CRM
   end
 
  	def call_option(user_selected)
-		@rolodex.add_contact if user_selected == "add"
-		@rolodex.modify_contact if user_selected == "modify"
-		@rolodex.delete_contact if user_selected == "delete"
-		@rolodex.display_all_contacts if user_selected == "display all"
-		@rolodex.display_particular_contact if user_selected == "display"
-		@rolodex.display_info_by_attribute if user_selected == "display attribute"
-		@rolodex.exit if user_selected == "exit"
+ 		case user_selected
+ 			when "add" then add_contact
+			when "modify" then modify_contact
+			when "delete" then delete_contact
+			when "display all" then display_all_contacts
+			when "display" then display_particular_contact
+			when "display attribute" then display_info_by_attribute
+			when "exit" then puts "Goodbye"
+			else 
+				puts "Please try again"
+				main_menu
+			end 
+	end
+
+	def add_contact
+		puts "Please put the contact first name"
+		first_name = gets.chomp
+		puts "Please put the contact last name"
+		last_name = gets.chomp
+		puts "Please put the contact email"
+		email = gets.chomp
+		puts "Please put any notes you want"
+		notes = gets.chomp
+		puts "Contact added"
+		contact = Contact.new(first_name, last_name, email, notes)
+		@rolodex.add_contact(contact)
+		main_menu
 	end 
-end
+
+	def modify_contact
+		puts "Please choose the contact ID of the contact you want to modify"
+		contact = gets.chomp.to_i
+		contact = contact - 1
+		puts "Please type what attribute you want to modify - [First Name, Last Name, Email, Notes]"
+		attribute = gets.chomp.to_s
+		puts "Are you sure you want to change the #{attribute.downcase} [yes, no]"
+		confirmation = gets.chomp
+		confirmation.downcase
+		if confirmation == "yes"
+			puts "What do you want to change it to?"
+			change = gets.chomp.to_s
+
+			case change
+				when "First Name" then @rolodex.contact_array[contact].first_name = change
+			  when "Last Name" then  @rolodex.contact_array[contact].last_name = change
+			  when "Email" then @rolodex.contact_array[contact].email = change
+				when "Notes" then @rolodex.contact_array[contact].notes = change
+			end 
+
+			puts "Your updated contact is 
+			\nFirst Name: #{rolodex.contact_array[contact].first_name} 
+			\nLast Name: #{@rolodex.contact_array[contact].last_name} 
+			\nEmail: #{@rolodex.contact_array[contact].email} 
+			\nNotes:#{@rolodex.contact_array[contact].notes}
+			"
+	 	else 
+	 		main_menu
+	 	end
+	 		main_menu
+	 end 
+
+	 def delete_contact
+	 end
+
+	 def display_all_contacts
+	 end
+
+	 def display_particular_contact
+	 end
+
+	 def display_info_by_attribute
+	 end 
+
+
+end 
 
 class Contact
 	attr_accessor :id, :first_name, :last_name, :email, :notes
@@ -52,12 +116,13 @@ class Contact
 	end
 
 	def to_sym
-		"#{rolodex.contact_array.index + 1}: #{@first_name} #{last_name} - #{email} - #{notes}"
+		"#{id}: #{@first_name} #{last_name} - #{email} - #{notes}"
 	end
 end
 
 class Rolodex
-	attr_accessor :contact_array
+	attr_accessor :contact_array, :contact_id 
+	@contact_id=1
 
 	def initialize
 		@contact_array = []
@@ -65,40 +130,13 @@ class Rolodex
 
 
 
-	def add_contact
-		puts "Please put the contact first name"
-		first_name = gets.chomp
-		puts "Please put the contact last name"
-		last_name = gets.chomp
-		puts "Please put the contact email"
-		email = gets.chomp
-		puts "Please put any notes you want"
-		notes = gets.chomp
-		@contact_array << Contact.new(first_name, last_name, email, notes)
-		@crm_name.main_menu
+	def add_contact(contact)	
+		@contact_array << contact
+		# contact.id = @contact_id
+		# @contact_id = @contact_id + 1
 	end
 
-	def modify_contact
-		puts "Please choose the contact ID of the contact you want to modify"
-		contact = gets.chomp
-		contact -= 1
-		puts "Please type what attribute you want to modify - [First Name, Last Name, Email, Notes]"
-		attribute = gets.chomp.to_s
-		puts "What do you want to change it to?"
-		change = gets.chomp
-
-		if attribute == "First Name"
-			puts @contact_array[contact]
-		elsif attribute == "Last Name"
-			@contact_array[contact]
-		elsif attribute == "Email"
-			@contact_array[contact]
-		elsif attribute == "Notes"
-			@contact_array[contact]
-		end 
-
-		crm1.main_menu
-
+	def modify_contact()
 	end
 
 	def display_all_contacts
@@ -122,7 +160,6 @@ class Rolodex
 
 	def to_s
 	end
-
 end 
 
 puts "Please input the CRM name"
